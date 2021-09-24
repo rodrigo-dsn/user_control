f5cd13a060b3e15b9b9babed6d7cbd77ce16f7d3
rodrigo-dsn <88155254+rodrigo-dsn@users.noreply.github.com>
2021-09-24 08:35:26 -0300

    Mudança na rota

--- library/library/urls.py
+++ library/library/urls.py
@@ -20,7 +20,7 @@ from rest_framework import routers
 from books.api import viewsets as booksviewsets
 
 route = routers.DefaultRouter()
-route.register(r'books/', booksviewsets.BooksViewSet, basename="Books")
+route.register(r'books', booksviewsets.BooksViewSet, basename="Books")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
